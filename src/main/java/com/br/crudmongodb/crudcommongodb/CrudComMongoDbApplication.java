aa6ccc893d4f5411682713ce6cfbbd9ff9fc48f5
jeanheberth <jeanheberth19@gmail.com>
2024-09-22 15:50:33 -0300

    Initial commit

---
+++ src/main/java/com/br/crudmongodb/crudcommongodb/CrudComMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.br.crudmongodb.crudcommongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudComMongoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudComMongoDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/br/crudmongodb/crudcommongodb/CrudComMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.crudmongodb.crudcommongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudComMongoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
